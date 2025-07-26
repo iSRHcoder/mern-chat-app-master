fac4054f13d73519a47a5f1fe5d2c8941f73de0c
Sunil Hatkadke <s.r.hatkadke@gmail.com>
2025-07-26 13:44:31 +0530

    deleted unnecessory code

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,15 +1,6 @@
 // connectToMongoDB.js
 import mongoose from "mongoose";
 
-// const connectToMongoDB = () => {
-//   console.log("MONGO_DB_URI:", process.env.MONGO_DB_URI);
-
-//   return mongoose.connect(process.env.MONGO_DB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
-// };
-
 const connectToMongoDB = () => {
   return mongoose.connect(process.env.MONGO_DB_URI); // No need for options
 };
